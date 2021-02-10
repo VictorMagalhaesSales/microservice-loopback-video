ce73b9f4deff7980998d4c103dc7fabdf08cfc57
Victor Magalhães <victormagalhaessales@gmail.com>
2021-02-10 13:55:41 -0300

    Instalando datasource de elasticsearch para ajudar no ORM

---
+++ src/datasources/esv7.datasource.ts
@@ -0,0 +1,28 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'esv7',
+  connector: 'esv6',
+  index: 'catalog',
+  apiVersion: '7',
+  defaultSize: ''
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class Esv7DataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'esv7';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.esv7', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
