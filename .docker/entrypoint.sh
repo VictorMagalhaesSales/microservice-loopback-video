9bc87520f0048822fb2555930462e3753271d948
Victor Magalhães <victormagalhaessales@gmail.com>
2021-02-20 13:23:20 -0300

    Fix: erro de permissão ao subir container da app

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -5,4 +5,7 @@ npm config set cache /home/node/app/.npm-cache --global
 
 cd /home/node/app
 npm install
+ls
+chmod +x /home/node/app/node_modules/.bin/lb-clean
+chmod +x /home/node/app/node_modules/.bin/lb-tsc
 nodemon -L #npm start
