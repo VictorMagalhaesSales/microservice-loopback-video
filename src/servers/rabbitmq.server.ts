50a3297a03968a5ab8e326ab4c855e99221e217f
Victor Magalhães <victormagalhaessales@gmail.com>
2021-02-23 03:26:33 -0300

    Update e delete - category

--- src/servers/rabbitmq.server.ts
+++ src/servers/rabbitmq.server.ts
@@ -7,6 +7,7 @@ import {CategoryRepository} from '../repositories';
 export class RabbitmqServer extends Context implements Server {
   listening: boolean;
   conn: Connection;
+  channel: Channel;
 
   constructor(
     @repository(CategoryRepository) private categoryRepo: CategoryRepository
@@ -30,25 +31,29 @@ export class RabbitmqServer extends Context implements Server {
 
   async boot(): Promise<void> {
     // Criação do canal para comunicação com o Rabbitmq que usaremos para enviar e consumir informações
-    const channel: Channel = await this.conn.createChannel();
+    this.channel = await this.conn.createChannel();
     // Criando 'exchange' e 'queue' e fazendo um bind através de uma routing key
-    const exchangeTopic = await channel.assertExchange('amq.topic', 'topic');
-    const syncVideosQueue = await channel.assertQueue('micro-catalog/sync-videos');
+    const exchangeTopic = await this.channel.assertExchange('amq.topic', 'topic');
+    const syncVideosQueue = await this.channel.assertQueue('micro-catalog/sync-videos');
     // Qualquer publicação na exchange amq.topic com a key seguindo o padrão micro.*.*, cairá na queue de sync videos
-    channel.bindQueue(syncVideosQueue.queue, exchangeTopic.exchange, 'micro.*.*');
+    this.channel.bindQueue(syncVideosQueue.queue, exchangeTopic.exchange, 'micro.*.*');
 
-    channel.consume(syncVideosQueue.queue, msg => {
+    this.channel.consume(syncVideosQueue.queue, msg => {
       if (!msg) return;
       const data = JSON.parse(msg.content.toString());
       const [category, operation] = msg.fields.routingKey.split('.').slice(1);
-      this.operacao(category, operation, data);
+      this.operacao(category, operation, data)
+        .then(() => this.channel.ack(msg))
+        .catch(() => this.channel.reject(msg));
     })
   }
 
   async operacao(category: string, operation: string, data: any) {
     if (category == 'video' && data) {
       switch (operation) {
-        case 'create': this.categoryRepo.create(data);
+        case 'create': await this.categoryRepo.create(data);
+        case 'update': await this.categoryRepo.updateById(data.id, data);
+        case 'delete': await this.categoryRepo.deleteById(data.id);
       }
     }
   }
